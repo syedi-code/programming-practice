35ab70635ff600d2291e0b9cee795223e20ec5d4
syedi-ui <84479429+syedi-ui@users.noreply.github.com>
2021-08-18 22:37:44 -0700

    Create container-with-most-water.cpp

---
+++ container-with-most-water.cpp
@@ -0,0 +1,24 @@
+/****
+** Problem: Container With Most Water (https://leetcode.com/problems/container-with-most-water/submissions/)
+** Runtime: 84 ms, faster than 60.98% of C++ online submissions for Container With Most Water.
+** Memory Usage: 59 MB, less than 79.95% of C++ online submissions for Container With Most Water.
+****/
+
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int area = 0;
+        int begin = 0;
+        int end = height.size() - 1;
+        
+        while (begin < end) {
+            int h = min(height[begin], height[end]);
+            area = max(area, h * (end - begin));
+            
+            while (height[begin] <= h && begin < end) begin++;
+            while (height[end] <= h && begin < end) end--;
+        }
+        
+        return area;
+    }
+};
